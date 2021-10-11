a537cb5b5886d132ab5020c2c0c8b7618e228a4d
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-10-11 21:33:27 +0300

    test:second

--- math_series/series.py
+++ math_series/series.py
@@ -4,3 +4,12 @@ def fibonacci(n):
         return (n)
     else:
         return (fibonacci(n-1) + fibonacci(n-2))
+
+
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return (lucas(n-1) + lucas(n-2))
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,8 +1,14 @@
 from math_series.series import fibonacci
-# from math_series.series import lucas
+from math_series.series import lucas
 
 
 def test_fibonacci():
     actual = fibonacci(9)
     excepted = 34
+    assert actual == excepted
+
+
+def test_lucas():
+    actual = lucas(9)
+    excepted = 76
     assert actual == excepted
